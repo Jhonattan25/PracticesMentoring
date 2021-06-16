6d80a11bcd57a72faa6ef2799e2fb66141500457
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-06-15 21:06:48 -0500

    add the function to paint chessboard

--- Chess/scripts/index.js
+++ Chess/scripts/index.js
@@ -1,10 +1,11 @@
-let chessBoard = buildChessBoard(8, 20);
+let chessBoard = buildChessBoard(8, 8);
+paintChessBoard(chessBoard);
 console.log(chessBoard);
 
 function buildChessBoard(rows, columns){
     let board = Array(rows);
-    let piecesB = ['B-T', 'B-N', 'B-P', 'B-K', 'B-Q', 'B-P', 'B-N', 'B-T'];
-    let piecesW = ['W-T', 'W-N', 'W-P', 'W-K', 'W-Q', 'W-P', 'W-N', 'W-T'];
+    let piecesB = ['&#x265C;', '&#x265E;', '&#x265D;', '&#x265B;', '&#x265A;', '&#x265D;', '&#x265E;', '&#x265C;'];
+    let piecesW = ['&#x2656;', '&#x2658;', '&#x2657;', '&#x2655;', '&#x2654;', '&#x2657;', '&#x2658;', '&#x2656;'];
 
     for(let i = 0; i < rows / 2; i++){
         board[i] = [];
@@ -20,9 +21,9 @@ function buildChessBoard(rows, columns){
                         board[rows - i - 1] = board[rows - i - 1].concat(piecesW);
                         j = (columns / 2) + 3;
                     }else{
-                        board[i] = board[i].concat(',B-P,B-P'.repeat(4).split(','));
+                        board[i] = board[i].concat(',&#x265F;,&#x265F;'.repeat(4).split(','));
                         board[i].splice(0, 1);
-                        board[rows - i - 1] = board[rows - i - 1].concat((',W-P,W-P'.repeat(4).split(',')));
+                        board[rows - i - 1] = board[rows - i - 1].concat((',&#x2659;,&#x2659;'.repeat(4).split(',')));
                         board[rows - i - 1].splice(0, 1);
                         j = (columns / 2) + 3;
                     }
@@ -37,4 +38,30 @@ function buildChessBoard(rows, columns){
         }
     }
     return board;
+}
+
+function paintChessBoard(board){
+    let boardHTML = document.getElementById('contentBoard');
+    let elementRow;
+    let elementColumn;
+    let flag = true ;
+
+    for (let i = 0; i < board.length; i++) {
+        elementRow = document.createElement('div');
+        elementRow.setAttribute('class', 'row');
+        boardHTML.appendChild(elementRow);
+
+        for (let j = 0; j < board[0].length; j++) {
+            elementColumn = document.createElement('div');
+            elementColumn.setAttribute('class', 'column');
+
+            if(flag){
+                elementColumn.setAttribute('class', 'column dark');
+            }
+            elementColumn.innerHTML = board[i][j];
+            elementRow.appendChild(elementColumn);
+            flag = !flag;
+        }
+        flag = !flag;
+    }
 }

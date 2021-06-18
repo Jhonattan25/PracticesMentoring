aa535eef9e348cdc02016e3c832691246c1d49da
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-06-17 21:35:45 -0500

    Add the function to select the cell and another function to move the piece

--- Chess/scripts/index.js
+++ Chess/scripts/index.js
@@ -2,6 +2,10 @@ let chessBoard = buildChessBoard(8, 8);
 paintChessBoard(chessBoard);
 console.log(chessBoard);
 
+let selectedCell = null;
+let selectedCellX;
+let selectedCellY;
+
 function buildChessBoard(rows, columns){
     let board = Array(rows);
     let piecesB = ['&#x265C;', '&#x265E;', '&#x265D;', '&#x265B;', '&#x265A;', '&#x265D;', '&#x265E;', '&#x265C;'];
@@ -53,15 +57,47 @@ function paintChessBoard(board){
 
         for (let j = 0; j < board[0].length; j++) {
             elementColumn = document.createElement('div');
+            elementColumn.setAttribute('id', `${i}${j}`);
             elementColumn.setAttribute('class', 'column');
 
-            if(flag){
+            if(flag)
                 elementColumn.setAttribute('class', 'column dark');
+
+            if(board[i][j] != ''){
+                elementColumn.setAttribute('onclick', `selectCell(${i}, ${j})`);
+            }else{
+                elementColumn.setAttribute('onclick', `movePiece(${i}, ${j})`);
             }
+            
             elementColumn.innerHTML = board[i][j];
             elementRow.appendChild(elementColumn);
             flag = !flag;
         }
         flag = !flag;
     }
+}
+
+function selectCell(x, y){
+    if(selectedCell != null){
+        selectedCell.style.backgroundColor = null;
+    }
+    selectedCellX = x;
+    selectedCellY = y;
+    selectedCell = document.getElementById(`${x}${y}`);
+    selectedCell.style.backgroundColor = 'blue';
+}
+
+function movePiece(x, y){
+    let item = document.getElementById(`${x}${y}`);
+    item.innerHTML = selectedCell.innerHTML;
+    selectedCell.innerHTML = '';
+
+    chessBoard[x][y] = chessBoard[selectedCellX][selectedCellY];
+    chessBoard[selectedCellX][selectedCellY] = '';
+
+    selectedCell.style.backgroundColor = null;
+    selectedCell.setAttribute('onclick', `movePiece(${selectedCellX}, ${selectedCellY})`);
+    selectedCell = null;
+    item.setAttribute('onclick', `selectCell(${x}, ${y})`);
+    console.log(chessBoard);
 }
